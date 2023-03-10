dd13cc32fdcd357ed66990930a92749f2be3b099
edsonrego <edson.rego1.ibm.com>
2023-03-10 16:47:48 -0300

    Routes

--- frontweb/src/App.tsx
+++ frontweb/src/App.tsx
@@ -1,12 +1,12 @@
-import Home from 'pages/Home';
-
 import './assets/styles/custom.scss';
 import './App.css';
 
+import Routes from 'routes';
+
 const App = () => {
   return (
-    <Home />
+    <Routes />
   );
-};
+}
 
 export default App;
